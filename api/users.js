d488986e95a58cc338bef8753c2734a469be1e4d
Brittanie <brittanie.staton@gmail.com>
2019-10-26 20:48:12 -0400

    user routes

--- api/users.js
+++ api/users.js
@@ -14,17 +14,68 @@ router.get('/testusers', (req, res,err) => res.json("users Works"));
 
 
 /**
- * @route	GET  api/users/
- * @desc	Get all users
+ * @route	GET  api/users/login
+ * @desc	Get all users (admins/superadmins only)
  * @access	public
 */
-router.get('/', (req, res) => {
+router.get('/admins', (req, res) => {
   let sql = 'SELECT * from users';
 
   pool.query(sql, (err, results) => {
     if(err) throw err;
     res.send(results);
   })  
-})
+});
+
+/**
+ * @route	GET  api/users/user:id
+ * @desc	Get specific user
+ * @access	public
+*/
+router.get('/login', (req, res) => {
+  let sql = 'SELECT * from users WHERE user_id = 5555';
+
+  pool.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(results);
+  })  
+});
+
+
+/**
+ * @route	POST  api/users/registeruser
+ * @desc	create a user
+ * @access	public
+*/
+
+router.post('/registeruser', (req, res) => {
+  var sql = "INSERT INTO users (user_id, first_name, last_name, email, password, university_id) VALUES ('5555', 'Ronda', 'bland', 'rondabland@yahoo.com', 'windyday123', '3333')";
+
+  pool.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(results);
+    console.log("1 record inserted");
+  });
+
+});
+
+
+/**
+ * @route	DELETE  api/users/deleteuser
+ * @desc	delete a user
+ * @access	public
+*/
+router.delete('/deleteuser', (req, res) => {
+  let sql = "DELETE FROM users WHERE user_id = 5556"
+
+  pool.query(sql, (err, results) => {
+    if(err) throw err;
+    res.send(results);
+    console.log("1 record deleted");
+  });
+
+});
+
+
 
 module.exports = router;
